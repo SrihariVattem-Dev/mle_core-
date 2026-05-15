969faf31d65b6da17f45ecc2395dc5ed7d37cf7f
Dev <sriharivattem@markwave.ai>
2026-05-15 13:26:03 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mle_core",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "python-jose[cryptography]==3.5.0",
+        "jwcrypto==1.5.7"
+    ],
+)
